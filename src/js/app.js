e7ee97abd5f62b43454c5a7981245fb23d50d0eb
Yvener Dutervil <72567854+yvener41@users.noreply.github.com>
2024-04-11 01:07:29 +0000

    First commit

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,23 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>
+          ${
+            variables.name === null || variables.name === ""
+              ? (variables.name = "First")
+              : variables.name
+          }
+
+          ${
+            variables.lastName === null || variables.lastName === ""
+              ? (variables.lastName = "Last")
+              : variables.lastName
+          }
+          
+          
+          
+          
+          </h1>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
